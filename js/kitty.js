16c5880bd24e6a873de690c4702b4bb180560fd7
Stuart Hiller <hiller.stuart@gmail.com>
2015-07-20 15:11:18 -0700

    Basic skeleton

--- js/kitty.js
+++ js/kitty.js
@@ -1,1 +1,47 @@
 console.log("hello world");
+
+var Photo = function(imgPath) {
+  this.votes = 0;
+  this.imgPath = imgPath;
+}
+
+// Photo.prototype.voteTrack = function(){
+//   //when the pictture is clicked
+//   this.vote +=1;
+// };
+
+var Tracker = function(photos) {
+  this.photos = [];
+};
+
+Tracker.prototype.voting = function(){
+
+};
+
+Tracker.prototype.declareWinner = function(){
+
+};
+
+Tracker.prototype.declaring = function(){
+
+};
+
+Tracker.prototype.getRandomPhotos = function(){
+
+};
+
+Tracker.prototype.displayKittens = function(){
+
+};
+
+Tracker.prototype.updateTally = function(){
+
+};
+
+Tracker.prototype.resetResults = function(){
+
+};
+
+Tracker.prototype.castVote = function(){
+
+};
