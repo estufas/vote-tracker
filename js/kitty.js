adf0306209ac95119b1fcdba1b9cd91c870cc365
Stuart Hiller <hiller.stuart@gmail.com>
2015-07-20 17:27:13 -0700

    Random image generator working

--- js/kitty.js
+++ js/kitty.js
@@ -1,19 +1,36 @@
-console.log("hello world");
 
-var Photo = function(imgPath) {
-  this.votes = 0;
-  this.imgPath = imgPath;
-}
+//
+// var Photo = function(imgPath) {
+//   this.votes = 0;
+//   this.leftPic = leftPic;
+//   this.rightPic = rightPic;
+// }
 
 // Photo.prototype.voteTrack = function(){
 //   //when the pictture is clicked
 //   this.vote +=1;
 // };
-
+var photos = ['img/1.jpg','img/2.jpg','img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg', 'img/11.jpg','img/11.jpg','img/12.jpg', 'img/13.jpg', 'img/14.jpg' ]
 var Tracker = function(photos) {
-  this.photos = [];
+  //this.photos = [];
 };
 
+Tracker.prototype.getRandomPhotos = function(){
+var lNumber = Math.floor((Math.random() * (14 -1)) + 1);
+var rNumber = Math.floor((Math.random() * (14 -1)) + 1);
+if (rNumber === lNumber){
+    return this.getRandomPhotos()
+} else {
+  this.leftPic = photos[lNumber];
+  this.rightPic = photos[rNumber];
+}
+
+  var lefty = document.getElementById('lefty');
+  lefty.innerHTML = "<img src=" + photos[lNumber] + ">";
+  var righty = document.getElementById('righty');
+  righty.innerHTML = "<img src=" + photos[rNumber] + ">";
+  console.log(lefty);
+};
 Tracker.prototype.voting = function(){
 
 };
@@ -26,13 +43,6 @@ Tracker.prototype.declaring = function(){
 
 };
 
-Tracker.prototype.getRandomPhotos = function(){
-
-};
-
-Tracker.prototype.displayKittens = function(){
-
-};
 
 Tracker.prototype.updateTally = function(){
 
@@ -45,3 +55,6 @@ Tracker.prototype.resetResults = function(){
 Tracker.prototype.castVote = function(){
 
 };
+
+var tracker = new Tracker();
+tracker.getRandomPhotos();
