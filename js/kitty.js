8c83b96652f3ffcffc3e1a0de1d0a6daf9312148
Stuart Hiller <hiller.stuart@gmail.com>
2015-07-22 17:39:01 -0700

    Everything wired up right and chart working

--- js/kitty.js
+++ js/kitty.js
@@ -1,18 +1,49 @@
-$(document).ready(function(){
+
+  var photoAry = [
+    { name : 'img/1.jpg',
+    votes : 0 },
+    { name : 'img/2.jpg',
+    votes : 0 },
+    { name : 'img/3.jpg',
+    votes : 0 },
+    { name : 'img/4.jpg',
+    votes : 0 },
+    { name : 'img/5.jpg',
+    votes : 0 },
+    { name : 'img/6.jpg',
+    votes : 0 },
+    { name : 'img/7.jpg',
+    votes : 0 },
+    { name : 'img/8.jpg',
+    votes : 0 },
+    { name : 'img/9.jpg',
+    votes : 0 },
+    { name : 'img/10.jpg',
+    votes : 0 },
+    { name : 'img/11.jpg',
+    votes : 0 },
+    { name : 'img/12.jpg',
+    votes : 0 },
+    { name : 'img/13.jpg',
+    votes : 0 },
+    { name : 'img/14.jpg',
+    votes : 0 }
+  ];
+
+console.dir(photoAry);
+
 var Photo = function(path) {
   this.path = path;
   this.votes = 0;
 };
 
-var imgPath = "./img/";
-
-var photoAry = [];
-for (var i = 1; i < 15; i++) {
-  photoAry.push(new Photo(imgPath + i + ".jpg"));
+var Tracker = function() {
+  this.lNumber = 0;
+  this.rNumber = 0;
 };
-console.dir(photoAry);
+console.dir(Tracker);
 
-Photo.prototype.getRandomPhotos = function(){
+Tracker.prototype.getRandomPhotos = function() {
 
   $('#lefty').empty();
   $('#righty').empty();
@@ -24,68 +55,67 @@ Photo.prototype.getRandomPhotos = function(){
 var lNumber = Math.floor((Math.random() * (14 -1)) + 1);
 var rNumber = Math.floor((Math.random() * (14 -1)) + 1);
 if (rNumber === lNumber){
-    return this.getRandomPhotos()
+    return this.getRandomPhotos();
+}
+  $('#lefty').append("<img src=" + photoAry[lNumber].name + ">");
+  $('#righty').append("<img src=" + photoAry[rNumber].name + ">");
+  console.log(lefty);
+  this.lNumber = lNumber;
+  this.rNumber = rNumber;
+  console.log(photoAry[lNumber].votes);
+// tracker.renderChart();
 }
 
-  $('#lefty').append("<img src=" + photoAry[lNumber].path + ">");
-  $('#righty').append("<img src=" + photoAry[rNumber].path + ">");
-  console.log(lefty);
-};
-
-// function changeImg(imgDiv) {
-//   var img = '#' + imgDiv;
-//   $(img).empty();
-//   $(img).css("background", "");
-//   $(img).css("border", "");
-//
-//
-// var lNumber = Math.floor((Math.random() * (14 -1)) + 1);
-// var rNumber = Math.floor((Math.random() * (14 -1)) + 1);
-// if ( imgDiv === "righty") {
-//
-// } else {
-//   var x = $(img);
-//   console.log()
-// }
-// }
-//
-//
-//   $('#lefty').append("<img src=" + photoAry[lNumber].path + ">");
-//   $('#righty').append("<img src=" + photoAry[rNumber].path + ">");
-// }
-// changeImg("lefty");
-// changeImg("righty");
-
-function clickWinner() {
-  $('#lefty').click(function(e){
-    $('#lefty').css("background", "rgb(154, 8, 8)");
-    addVote(e);
-    });
-  $('#righty').click(function(e){
-    $('#righty').css("background", "rgb(154, 8, 8)");
-    addVote(e);
-  });
-};
-
-function addVote(e) {
+Tracker.prototype.addVote = function(e) {
   console.dir(e.target.parentElement);
   var targetValue = e.target.attributes[0].value;
   for(var i = 0; i < photoAry.length; i++) {
-    if( targetValue === photoAry[i].path) {
+    if( targetValue === photoAry[i].name) {
       photoAry[i].votes++;
+      console.log(photoAry[i].votes);
     }
   }
-  // if (e.target.parentElement.id == "lefty") {
-  //   changeImg("righty");
-  // } else {
-  //   changeImg("lefty");
-  // }
-}
+};
 
+Tracker.prototype.renderChart = function() {
+var barData = {
+    labels : ['left cat - right cat'],
+    datasets : [
+        {
+        fillColor : "#48A497",
+        strokeColor : "#48A4D1",
+        data : [photoAry[this.lNumber].votes]
+      },
+      {
+        fillColor : "#48A567",
+        strokeColor : "#4876D1",
+        data : [photoAry[this.rNumber].votes]
+      }
+    ]
+  }
+var myChart = document.getElementById('myChart').getContext("2d");
+new Chart(myChart).Bar(barData);
+};
 
+var tracker = new Tracker();
+tracker.getRandomPhotos();
 
-var photo = new Photo();
-photo.getRandomPhotos();
-clickWinner();
-addVote();
+$(document).ready(function(){
+
+  $('#lefty').click(function(e){
+    $('#lefty').css("background", "rgb(154, 8, 8)");
+    tracker.addVote(e);
+    console.log('click');
+    });
+  $('#righty').click(function(e){
+    $('#righty').css("background", "rgb(154, 8, 8)");
+    tracker.addVote(e);
+    console.dir(photoAry)
+    });
+  $('button').click(function(){
+    tracker.getRandomPhotos();
+    tracker.renderChart();
+    console.log('click-2');
+  });
+
 });
